0340b747623045173b9c900a2dcff3fa1b3d6d71
Bonnie Mack <78276424+bonnmccu@users.noreply.github.com>
2021-05-26 12:08:16 -0700

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,15 @@
+package mypack;
+
+public class Test 
+{
+		public static void main(String args[]) {
+			Employee e = new Employee(); //object creation
+			e.setName("Arjun"); //setting the value of the object
+			System.out.println(e.getName());
+		}
+		
+
+}
+
+
+}
